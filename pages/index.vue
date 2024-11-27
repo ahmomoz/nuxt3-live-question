5ac63d6610f3cca291e3dd3591585423cc3a8d72
ahmomoz <x5426854268@gmail.com>
2024-11-27 23:06:42 +0800

    feat: 引入 vue3-colorpicker 和 vue-loading-overlay 套件

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,44 @@
 <script setup>
+const { $useLoading } = useNuxtApp();
+
+const pureColor = ref("green");
+const gradientColor = ref(
+  "linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 100%)"
+);
+
+const loadingHandler = $useLoading({
+  backgroundColor: "red",
+  loader: "dots",
+  "is-full-page": false,
+});
+
 function openLoading() {
   // 開啟讀取效果
+  const loader = loadingHandler.show();
   setTimeout(() => {
     // 關閉讀取效果
+    loader.hide();
   }, 1000);
 }
 </script>
 
 <template>
+  <div class="container">
+    <h1>Nuxt3 整合 vue3-colorpicker 插件</h1>
+    <ClientOnly>
+      <ColorPicker
+        v-model:pureColor="pureColor"
+        v-model:gradientColor="gradientColor"
+      />
+    </ClientOnly>
+  </div>
+
   <button type="button" @click="openLoading">開啟 Loading 效果</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
--- plugins/loading.js
+++ plugins/loading.js
@@ -1,1 +1,7 @@
-export default defineNuxtPlugin((nuxtApp) => {});
+import { LoadingPlugin, useLoading } from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(LoadingPlugin);
+  nuxtApp.provide("useLoading", useLoading);
+});
